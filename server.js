a1a877268ed7bb6414118306716f594f58d1aab6
jayreb92 <87283593+jayreb92@users.noreply.github.com>
2024-05-10 19:00:25 -0400

    sigh

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.set("view engine", "ejs")
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
