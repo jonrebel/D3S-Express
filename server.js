baaf1fb66c166255fa0a7eb76e4784e916f29f34
jayreb92 <87283593+jayreb92@users.noreply.github.com>
2024-05-16 15:59:40 -0400

    paths

--- server.js
+++ server.js
@@ -49,13 +49,6 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
 
 app.get("/", (req,res) =>{
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
